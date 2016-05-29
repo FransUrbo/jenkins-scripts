7ddbf333276f748474db11556db7e14e50550faa
Turbo Fredriksson <turbo@bayour.com>
2016-05-29 19:55:34 +0100

    lxc-setup-devel-image.sh+image-setup.sh: New script - Setup build env.

---
+++ image-setup.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+# ========================================================================
+# This is the secondary script (of two) to setup a build environment.
+# It is intended to be executed inside the container and will install
+# and configure the configure the container to do package builds.
+#
+# Copyright 2016 Turbo Fredriksson <turbo@bayour.com>.
+# Released under GPL, version of your choosing.
+# ========================================================================
+
+if type apt-get > /dev/null 2>&1; then
+   adduser --uid 110 --disabled-login jenkins
+else
+   adduser --uid 110 -m jenkins
+fi
+mkdir ~jenkins/build
+chown jenkins:jenkins ~jenkins/build
+
+if [ -d /etc/sudoers.d ]; then
+   echo 'jenkins ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers.d/jenkins
+else
+   echo 'jenkins ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers
+fi
+
+cat <<EOF > /etc/dupload.conf
+package config;
+\$default_host = "celia";
+\$preupload{'changes'} = '/usr/share/dupload/gpg-check %1';
+\$cfg{'celia'} = {
+    fqdn => "celia.bayour.com",
+    login => "turbo",
+    method => "scp",
+    incoming => "/usr/src/incoming.jenkins",
+    dinstall_runs => 1,
+    nonus => 1
+};
+
+1;
+EOF
+
+if type dnf > /dev/null 2>&1; then
+   dnf install rpm-build autoconf automake libtool sudo git kernel-devel kernel-headers make
+elif type yum > /dev/null 2>&1; then
+   yum install rpm-build autoconf automake libtool sudo git kernel-devel kernel-headers make
+elif type apt-get > /dev/null 2>&1; then
+   # deb http://old-releases.ubuntu.com/ubuntu/ utopic main restricted universe multiverse
+   # deb http://old-releases.ubuntu.com/ubuntu/ utopic-proposed main restricted universe multiverse
+   # deb http://old-releases.ubuntu.com/ubuntu/ utopic-updates main restricted universe multiverse
+   # deb http://old-releases.ubuntu.com/ubuntu/ utopic-security main restricted universe multiverse
+
+   apt-get update
+   apt-get install -y build-essential autoconf automake \
+	   libtool sudo git make linux-headers-amd64 autogen debhelper \
+	   dupload dkms devscripts git-buildpackage iputils-ping
+   apt-get clean
+fi
+
+sed -i "s@^\(Defaults.*requiretty\)@#\1@" /etc/sudoers
---
+++ lxc-setup-devel-image.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# ========================================================================
+# This is the primary script (of two) to setup a build environment for LXC.
+#
+# This script depend on already downloaded images from "images:".
+#
+# Copyright 2016 Turbo Fredriksson <turbo@bayour.com>.
+# Released under GPL, version of your choosing.
+# ========================================================================
+
+set -ex
+
+if [ -z "${1}" ]; then
+    echo "Usage: $(basename "${0}") <dist>"
+    exit 1
+else
+    DIST="${1}"
+fi
+
+lxc launch ${DIST} ${DIST}
+lxc file push ~jenkins/scratch/image-setup.sh ${DIST}/tmp/
+lxc exec ${DIST} /tmp/docker-image-setup.sh
+lxc stop ${DIST} --force
+lxc snapshot ${DIST} ${DIST}-devel
+lxc copy ${DIST}/${DIST}-devel local:${DIST}-devel
+lxc delete ${DIST}
+lxc publish local:${DIST}-devel --alias ${DIST}-devel
+lxc delete ${DIST}-devel
+lxc image show ${DIST} | sed s@\(description:.*\)@\1 - devel install@ | lxc image edit ${DIST}-devel
